cb13427e3625d0bf18c64f573a3da58bb64d1127
Aaron Huang <aaronhuang@ucsb.edu>
2020-07-11 01:11:06 -0700

    AH - Abstract Class simple outline
    Abstract Class simple outline. We can add to them as we work on new features.

---
+++ Mayo Is Breadlube/Assets/Scripts/Layer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class for layers - ex: sky, ground, underground 
+abstract public class Layer
+{
+    public Tile[][] tileMap;
+    List<Player> players;
+    List<Enemies> enemies; 
+}
---
+++ Mayo Is Breadlube/Assets/Scripts/Tile.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Tile{
+    List<Entity> entitites;
+    List<Unit> units;
+}
