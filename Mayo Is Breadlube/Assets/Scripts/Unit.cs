5594019cdec0b75c56ae1fe4ea7f97b91af314cf
Ray <41882725+Ray160@users.noreply.github.com>
2020-07-19 19:57:46 -0700

    Update Unit.cs

--- Mayo Is Breadlube/Assets/Scripts/Unit.cs
+++ Mayo Is Breadlube/Assets/Scripts/Unit.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    final float pierceMult = 2.5;
+    final float bluntMult = 1.3;
+    public int actionPoints;
+    public int maxHp;
+    public int maxArmor;
+
     public int hp;
-    public int atk;
-    public int def;
-    public List<string> statuses;
-    public string armor;
-    public string weapon;
+    public int armor;
+
+    public bool guard;
+    public bool parry;
+    public float hpMult;
+    public int atkMult;
+
+    //public List<string> statuses;
+
+    //If theres enough probably worth making them into an array
+    public int poisonCounter;
+    public int stunCounter;
+    public int staggerCounter;
+    
+    //public string weapon;
+
+    public AbilityTree abilityTree;
+
+    public int takeNormalDamage(Damage damage){
+        if(damage.type = "pierce"){
+            if(armor > 0){
+                int calcArmor = damage.value / (hpMult * pierceMult);
+                int remainder = damage.value- calcArmor;
+                armor = mathf.Clamp((calcArmor - damage.value) * (hpMult * pierceMult), 0, maxArmor);
+
+                health = mathf.Clamp(health - remainder, 0, maxHp);
+            }else{
+                health = mathf.Clamp(health - damage.value, 0, maxHp);
+            }
+        }
+    }
 }
